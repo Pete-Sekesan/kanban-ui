eb4972cb66875454fd312ee1106de9e25d152ab0
Peter Sekesan <psekesan@gmail.com>
2021-04-27 13:41:52 -0400

    one drag and drop column created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
+
+const tasksFromBackend = [
+  {
+    id: uuidv4(),
+    title: "Get MongoDB up and running",
+    description: "Figure out issue with MacOSX Catalina",
+    assignee: "Pete",
+  },
+  {
+    id: uuidv4(),
+    title: "Record Demo Video",
+    description: "Submit demo video explaining code and issues ran into",
+    assignee: "Pete",
+  },
+];
+
+const columnsFromBackend = [
+  {
+    [uuidv4()]: {
+      name: "Todo",
+      tasks: [tasksFromBackend],
+    },
+  },
+];
 function App() {
+  const [columns, setColumns] = useState(columnsFromBackend);
+
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>KANBAN!</h1>
       </header>
-    </div>
+      <div className="App-list">
+        <DragDropContext onDropEnd={(result) => console.log(result)}>
+          {Object.entries(columns).map(([id, column]) => {
+            return (
+              <Droppable droppableId={id}>
+                {(provided, snapshot) => {
+                  return (
+                    <div
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                      style={{
+                        background: snapshot.isDraggingOver
+                          ? "lightblue"
+                          : "lightgrey",
+                        padding: 4,
+                        width: 250,
+                        minHeight: 500,
+                      }}
+                    ></div>
+                  );
+                }}
+              </Droppable>
+            );
+          })}
+        </DragDropContext>
+
+        {/* {store.lists.map((list) => (
+          <List
+            key={list.id}
+            header={list.header}
+            cards={list.cardIds.map((id) => store.allCards[id])}
+          />
+        ))} */}
+      </div>
+    </main>
   );
 }
 
